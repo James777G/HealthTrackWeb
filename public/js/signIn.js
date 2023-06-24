176423708b6a23a907205b70dd574ca81d9e008b
James777G <hgon777@aucklanduni.ac.nz>
2023-06-25 00:39:41 +1200

    adjustments

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -2,11 +2,14 @@ const { response } = require("express");
 
 function signInAction() {
 
-    const username = document.getElementById('username');
-    const password = document.getElementById('password');
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
     axios
-        .post('/signin', { username, password })
+        .post('http://localhost:8083/login', {
+            username : username,
+            password : password
+        })
         .then(response => {
             alert(response.data)
         })
